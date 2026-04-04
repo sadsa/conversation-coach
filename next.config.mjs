807257d0c35b449e60c99d98e2f83ba585560e02
Josh Biddick <joshua.biddick@entelect.co.nz>
2026-04-04 22:58:06 +1300

    feat: inject NEXT_PUBLIC_BUILD_DATE at build time

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BUILD_DATE: new Date().toISOString().split('T')[0],
+  },
+};
 
 export default nextConfig;
