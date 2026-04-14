841e91c70a79c00d37dde9feeccac1f0a4822837
Josh Biddick <joshua.biddick@entelect.co.nz>
2026-04-15 09:44:14 +1200

    chore: add data-loss comment to drop-leitner migration

--- supabase/migrations/20260415000000_drop_leitner_columns.sql
+++ supabase/migrations/20260415000000_drop_leitner_columns.sql
@@ -1,4 +1,5 @@
 -- Drop Leitner box columns from practice_items table
+-- Destructive: existing leitner_box and leitner_due_date data is permanently removed.
 ALTER TABLE practice_items
   DROP COLUMN IF EXISTS leitner_box,
   DROP COLUMN IF EXISTS leitner_due_date;
