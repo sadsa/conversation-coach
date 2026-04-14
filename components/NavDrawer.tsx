299e000d9abb744c6b6a5c1bf6616f3ec658f98b
Josh Biddick <joshua.biddick@entelect.co.nz>
2026-04-15 09:51:12 +1200

    chore: remove flashcards nav tab

--- components/NavDrawer.tsx
+++ components/NavDrawer.tsx
@@ -39,19 +39,6 @@ const TABS = [
     ),
   },
   {
-    href: '/flashcards',
-    labelKey: 'nav.flashcards',
-    exact: false,
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-        stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"
-        className="w-5 h-5 flex-shrink-0" aria-hidden="true">
-        <rect x="2" y="4" width="20" height="16" rx="2" ry="2" />
-        <line x1="2" y1="10" x2="22" y2="10" />
-      </svg>
-    ),
-  },
-  {
     href: '/insights',
     labelKey: 'nav.insights',
     exact: false,
